0cc46d919f43e50e4488298cdba454924a4f3efd
Lautaro Meiners <lautaromeiners@protonmail.com>
2022-05-08 15:34:19 -0300

    Adds models

--- ecommerce/api/models.py
+++ ecommerce/api/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    stock = models.IntegerField()
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    date_time = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        verbose_name = 'Order'
+        verbose_name_plural = 'Orders'
+    
+    def __str__(self):
+        return str(self.date_time)
+
+
+class OrderDetail(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_detail')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    class Meta:
+        verbose_name = 'Order Detail'
+        verbose_name_plural = 'Order Details'
+
+    def __str__(self):
+        return f'{self.order} has {self.product}'
